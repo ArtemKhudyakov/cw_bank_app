74274d7bdcb12a720f3168addbcf20ff4d6c5820
Dr.T. <Aohudyakov@gmail.com>
2025-03-25 11:03:45 +0500

    подготовлен модуль datareader и функция чтения данных

---
+++ src/data_reader.py
@@ -0,0 +1,23 @@
+import csv
+import pathlib as p
+from typing import Any, Dict, Hashable, List
+
+import pandas as pd
+
+
+def xlsx_reader(path_to_xlsx_file: str = "data/operations.xlsx",) -> List[Dict[Hashable, Any]]:
+    """Функция принимает путь до файла данных формата xlsx, и возвращает список
+    словарей"""
+    current_file_path = p.Path(__file__).resolve()
+    project_root_path = current_file_path.parent.parent
+    file_path = f"{project_root_path}/{path_to_xlsx_file}"
+    try:
+        excel_data = pd.read_excel(file_path)
+        data_list = excel_data.to_dict(orient="records")
+        return data_list
+    except FileNotFoundError:
+        print("File not found")
+        raise FileNotFoundError
+
+
+
